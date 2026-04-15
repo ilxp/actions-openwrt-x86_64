d205628a2a8235a945968378e8fb64b7d5447301
ilxp <61609258+ilxp@users.noreply.github.com>
2026-04-15 09:28:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.201/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # Modify rust
 #sed -i '78s/true/false/' feeds/packages/lang/rust/Makefile
